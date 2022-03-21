b802738731e81bfdc3f4f9ba0c057fa358b6cbe5
Imran Hossain <ih060899@users.noreply.github.com>
2022-03-21 21:24:42 +0600

    initial commit

---
+++ src/main/java/com/imran/usersystem/UserManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.imran.usersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSystemApiApplication.class, args);
+	}
+
+}
